df35d383592f39de6eb9dc0fb32cb32414aedf4a
Febrandt <fernandombrandt@gmail.com>
2025-01-03 19:39:40 -0300

    feat: search endpoint

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ app = FastAPI()
 taco = pd.read_csv("data/taco.csv", encoding = 'utf8')
 amino = pd.read_csv("data/amino.csv", encoding = 'utf8')
 
-@app.get("/search")
-async def create(name: str):
-    return loads(taco[taco['name'].str.contains(name,na=False, case=False)].to_json(orient='records'))
+@app.get('')
+
+@app.get('/search')
+async def create(name: str, min_calories: int = 0, max_calories: int = 10000):
+    filter_names = taco[taco.name.str.contains(name,na=False, case=False)]
+    filter_min_calories = filter_names[filter_names.kcal.astype(int) >= min_calories]
+    filter_max_calories = filter_min_calories[filter_names.kcal.astype(int) <= max_calories]
+    end = filter_max_calories
+    return loads(end.to_json(orient='records'))
 
